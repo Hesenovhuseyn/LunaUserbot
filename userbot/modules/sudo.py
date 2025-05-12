6ea26ea9ee809a87e63d5bc3e8d72df3443aa2bf
huseyn hesenov <huseyn-heseniv-2022@mail.ru>
2025-05-12 20:20:22 +0400

    ,

--- userbot/modules/sudo.py
+++ userbot/modules/sudo.py
@@ -7,7 +7,7 @@ from userbot.events import register
 
 @register(incoming=True, from_users=SUDO_ID, pattern="^.salive$")
 async def _(q):
-    await q.client.send_message(q.chat_id,"`[⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝](https://t.me/silgiub)ＳＵＤＯ ✨`")
+    await q.client.send_message(q.chat_id,"`[❤️𝑳𝒖𝒏𝒂𝑼𝒔𝒆𝒓𝒃𝒐𝒕❤️](https://t.me/lunadestek)ＳＵＤＯ ✨`")
 
 CmdHelp('sudo').add_command(
     'salive', None, 'SUDOnun aktiv olub olmadığını yoxlayar.'
